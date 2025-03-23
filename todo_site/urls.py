f4f3cecb9b070362d5af11e7b4a3b50c2ad64cba
cr7 <jmcristian2@gmail.com>
2025-03-23 12:07:54 -0500

    App de apuntes

---
+++ todo_site/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from todo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='todo'),
+    path('del/<str:item_id>', views.remove, name="del"),
+]
